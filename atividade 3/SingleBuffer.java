a90396920a0bb11915219d03a883c0d82ef5ac11
Vitória Camelo <93448845+VitoriaCamelo@users.noreply.github.com>
2024-03-28 18:23:34 -0300

    Update SingleBuffer.java

--- atividade 3/SingleBuffer.java
+++ atividade 3/SingleBuffer.java
@@ -2,26 +2,29 @@ import java.util.ArrayList;
 import java.util.concurrent.Semaphore;
 
 public class SingleBuffer{
-  Semaphore sprodutor;
+  Integer sleituras;
   ArrayList<String> value;
 
   public SingleBuffer(){
-    sprodutor = new Semaphore(1);
     value = new ArrayList<String>();
+    sleituras = 0;
   }
   public void deposit(String v){
+    sleituras = 4;
     value.add(v);
   }
-  public void fetch(){
-    sleituras--;
+  public void fetch(Semaphore s){
+    sleituras = sleituras-1;
     String mensagem = "";
     for(int i=0; i<value.size(); i++){
       mensagem += value.get(i);
       mensagem += " ";
     }
-    System.out.println(mensagem);
-    if(sleituras==0)
+    System.out.println(mensagem+"\n");
+    if(sleituras==0){
       value = new ArrayList<String>();
+      s.release();
+    }
     
   }
   
