8693dc4fb93c98c72c8a89963b85ea8253ac3f48
Vitória Camelo <93448845+VitoriaCamelo@users.noreply.github.com>
2024-03-28 18:22:23 -0300

    Update Consumer.java

--- atividade 3/Consumer.java
+++ atividade 3/Consumer.java
@@ -1,16 +1,21 @@
 import java.util.concurrent.*;
-/*
-- As threads consumidoras deverão checar o buffer, ler a última palavra não lida no buffer e imprimi-la na tela identificando seu próprio Thread ID;
-*/
+
 public class Consumer extends Thread{
   SingleBuffer buffer;
   String value;
+  Semaphore s1;
+  Semaphore s2;
 
-  public Consumer(SingleBuffer b){
+  public Consumer(SingleBuffer b, Semaphore s, Semaphore t){
     buffer = b;
+    s1 = s;
+    s2 = t;
   }
   public void run(){
-    while(true)
-      buffer.fetch();
+    while(true){
+      try{s2.wait();}catch(Exception e){}
+      buffer.fetch(s1);
+      try{s2.release();}catch(Exception e){}
+    }
   }
 }
