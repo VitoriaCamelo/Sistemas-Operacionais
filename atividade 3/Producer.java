8137454377bc2973fa5cc4ba5eab5a37c14e3780
Vitória Camelo <93448845+VitoriaCamelo@users.noreply.github.com>
2024-04-04 11:06:05 -0300

    Update Producer.java

--- atividade 3/Producer.java
+++ atividade 3/Producer.java
@@ -7,19 +7,24 @@ public class Producer extends Thread{
   Semaphore s2;
 
   public Producer(SingleBuffer b, String[] v, Semaphore s, Semaphore t){
+    // produtor recebe buffer, mensagem para emviar e os semáforos s1 e s2
     buffer = b;
     value = v;
     s1 = s;
     s2 = t;
   }
   public void run(){
+    // s1.acquire() garante que 1 produtor execute por vez
     try{s1.acquire();}catch(Exception e){}
-    for(int j=0; j<value.length; j++)
+    // loop para enviar cada palavra da mensagem
+    for(int j=0; j<value.length; j++){
+      // envio da palavra
       buffer.deposit(value[j]);
+      // espera
       try{Thread.sleep(2000);}
-      catch (InterruptedException e) {
-        System.out.println("Thread interrupted");
+      catch(InterruptedException e){}
     }
+    // produtor libera consumidores
     try{s2.release();}catch(Exception e){}
   }
 }
